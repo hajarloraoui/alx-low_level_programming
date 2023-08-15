43aef6ce2e381e6936a9708cb26bb75f9c21ea86
HajarLoraoui <hajar.loraoui12@gmail.com>
2023-08-15 13:28:39 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<main.h>
+/**
+ * main - check the code
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
